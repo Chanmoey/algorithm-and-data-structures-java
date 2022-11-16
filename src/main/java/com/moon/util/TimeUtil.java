9f74efbc577c63ed1becea10d101b4eb04dfb53f
Chanmoey <relijifa@gmail.com>
2022-11-17 01:55:25 +0800

    封装纳秒转秒的逻辑。

---
+++ src/main/java/com/moon/util/TimeUtil.java
@@ -0,0 +1,14 @@
+package com.moon.util;
+
+/**
+ * @author Chanmoey
+ * @date 2022年11月16日
+ */
+public class TimeUtil {
+
+    public static final double NANOSECOND2SECOND = 1000000000.0;
+
+    public static double nanoSecond2Second(long startTime, long endTime) {
+        return (endTime - startTime) / NANOSECOND2SECOND;
+    }
+}

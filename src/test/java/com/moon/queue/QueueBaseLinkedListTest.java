e0a40b02ef8ce5d7a959b9d691532fc886932698
Chanmoey <relijifa@gmail.com>
2022-11-17 01:55:43 +0800

    测试队列的实现。

---
+++ src/test/java/com/moon/queue/QueueBaseLinkedListTest.java
@@ -0,0 +1,25 @@
+package com.moon.queue;
+
+import org.junit.Test;
+
+/**
+ * @author Chanmoey
+ * @date 2022年11月16日
+ */
+public class QueueBaseLinkedListTest {
+
+    @Test
+    public void test() {
+        QueueBaseLinkedList<Integer> queue = new QueueBaseLinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}

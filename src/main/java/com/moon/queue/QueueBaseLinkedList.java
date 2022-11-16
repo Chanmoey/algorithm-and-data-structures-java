1c3ceda4fbaaad82dee8d9dfba2ab98b78281022
Chanmoey <relijifa@gmail.com>
2022-11-17 01:54:54 +0800

    为链表添加尾指针，这样可以方便实现队列的逻辑。

---
+++ src/main/java/com/moon/queue/QueueBaseLinkedList.java
@@ -0,0 +1,46 @@
+package com.moon.queue;
+
+import com.moon.linkedlist.LinkedListWithTail;
+
+/**
+ * @author Chanmoey
+ * @date 2022年11月16日
+ */
+public class QueueBaseLinkedList<E> implements Queue<E>{
+
+    private final LinkedListWithTail<E> list;
+
+    public QueueBaseLinkedList() {
+        this.list = new LinkedListWithTail<>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        list.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return list.getFist();
+    }
+
+    @Override
+    public String toString() {
+        return "Queue: front " + list + " tail";
+    }
+}

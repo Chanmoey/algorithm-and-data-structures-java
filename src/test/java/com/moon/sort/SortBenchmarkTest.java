f93c3b409744f4636e437cbb56c7156ff050e0e4
Chanmoey <relijifa@gmail.com>
2022-11-22 01:32:35 +0800

    实现双路快速排序，三路快速排序。

--- src/test/java/com/moon/sort/SortBenchmarkTest.java
+++ src/test/java/com/moon/sort/SortBenchmarkTest.java
@@ -13,6 +13,8 @@ public class SortBenchmarkTest {
     private static final String SELECTION = "com.moon.sort.selectionsort.SelectionSort";
     private static final String INSERTION = "com.moon.sort.insertionsort.InsertionSort";
     private static final String MERGE = "com.moon.sort.mergesort.MergeSort";
+    private static final String QUICK = "com.moon.sort.quicksort.QuickSort";
+    private static final String JAVA = "java.util.Arrays";
 
     @Test
     public void twoSortWithOrderArrayBenchmark() {
@@ -20,10 +22,8 @@ public class SortBenchmarkTest {
         for (int n : dataSize) {
             Integer[] arr = ArrayGenerator.generateOrderedArray(n);
             Integer[] arr2 = Arrays.copyOf(arr, arr.length);
-            SortingHelper.sortTest("com.moon.sort.selectionsort.SelectionSort",
-                    "sort", Comparable[].class, arr);
-            SortingHelper.sortTest("com.moon.sort.insertionsort.InsertionSort",
-                    "sortBetter", Comparable[].class, arr2);
+            SortingHelper.sortTest("com.moon.sort.selectionsort.SelectionSort", "sort", Comparable[].class, arr);
+            SortingHelper.sortTest("com.moon.sort.insertionsort.InsertionSort", "sortBetter", Comparable[].class, arr2);
         }
     }
 
@@ -37,6 +37,19 @@ public class SortBenchmarkTest {
             SortingHelper.sortTest(SELECTION, "sort", Comparable[].class, arr);
             SortingHelper.sortTest(INSERTION, "sortBetter", Comparable[].class, arr2);
             SortingHelper.sortTest(MERGE, "sort", Comparable[].class, arr3);
+        }
+    }
+
+    @Test
+    public void nlognSortBenchMark() {
+        int[] dataSize = {10000, 100000, 1000000, 10000000, 100000000};
+        for (int n : dataSize) {
+            Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+            Integer[] arr1 = Arrays.copyOf(arr, arr.length);
+            Integer[] arr2 = Arrays.copyOf(arr, arr.length);
+            SortingHelper.sortTest(MERGE, "sortBetter", arr);
+            SortingHelper.sortTest(QUICK, "sortBetter", arr1);
+            SortingHelper.sortTest(JAVA, "sort", Object[].class, arr2);
         }
     }
 }

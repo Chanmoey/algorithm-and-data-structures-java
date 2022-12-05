daa1ddae2692656b248bb375da9c879f704797ca
Chanmoey <relijifa@gmail.com>
2022-12-06 01:43:17 +0800

    通过堆实现优先队列。

---
+++ src/main/java/com/moon/queue/PriorityQueue.java
@@ -0,0 +1,47 @@
+package com.moon.queue;
+
+import com.moon.heap.MaxHeap;
+
+/**
+ * 优先队列
+ *
+ * @author Chanmoey
+ * @date 2022年12月06日
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private final MaxHeap<E> maxHeap;
+
+    public PriorityQueue(int capacity) {
+        this.maxHeap = new MaxHeap<>(capacity);
+    }
+
+    public PriorityQueue() {
+        this.maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.remove();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.peek();
+    }
+}

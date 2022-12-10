f7acc602be846053c4895200c58135f657ec8191
Chanmoey <relijifa@gmail.com>
2022-12-11 01:36:57 +0800

    添加线段树的查询、更新操作。

--- src/main/java/com/moon/segmenttree/SegmentTree.java
+++ src/main/java/com/moon/segmenttree/SegmentTree.java
@@ -6,9 +6,9 @@ package com.moon.segmenttree;
  */
 public class SegmentTree<E> {
 
-    private Merger<E> merger;
-    private E[] data;
-    private E[] tree;
+    private final Merger<E> merger;
+    private final E[] data;
+    private final E[] tree;
 
     @SuppressWarnings("unchecked")
     public SegmentTree(E[] arr, Merger<E> merger) {
@@ -62,9 +62,72 @@ public class SegmentTree<E> {
         return (index << 1) + 2;
     }
 
+    public E query(int queryL, int queryR) {
+        checkQueryBound(queryL, queryR);
+
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    /**
+     * 在以treeIndex为根的线段树中[l...r]的范围内，搜素区间[queryL...queryR]的值
+     */
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (queryL >= mid + 1) {
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        } else if (queryR <= mid) {
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        }
+
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        return merger.merge(leftResult, rightResult);
+    }
+
+    /**
+     * 将index位置的值更新成e
+     */
+    public void set(int index, E e) {
+        checkIndex(index);
+    }
+
+    /**
+     * 在以treeIndex为根的线段是中，更新index的值为e
+     */
+    private void set(int treeIndex, int l, int r, int index, E e) {
+        if (l == r) {
+            tree[treeIndex] = e;
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if (index >= mid + 1) {
+            set(rightTreeIndex, mid + 1, r, index, e);
+        } else {
+            set(leftTreeIndex, l, mid, index, e);
+        }
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+    }
+
     private void checkIndex(int index) {
         if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("Index is illegal!");
+        }
+    }
+
+    private void checkQueryBound(int l, int r) {
+        if (l < 0 || l >= data.length || r < 0 || r >= data.length || l > r) {
+            throw new IllegalArgumentException("Query bound is illegal.");
         }
     }
 
--- src/test/java/com/moon/segmenttree/SegmentTreeTest.java
+++ src/test/java/com/moon/segmenttree/SegmentTreeTest.java
@@ -14,4 +14,13 @@ public class SegmentTreeTest {
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, Integer::sum);
         System.out.println(segmentTree);;
     }
+
+    @Test
+    public void queryTest() {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, Integer::sum);
+        System.out.println(segmentTree.query(0, 2));
+        System.out.println(segmentTree.query(2, 5));
+        System.out.println(segmentTree.query(0, 5));
+    }
 }

8dd9fc334d8d2bb23f8a4d7c4ebcabd7869e7c27
Chanmoey <relijifa@gmail.com>
2022-12-18 01:45:03 +0800

    实现AVLTree的左旋转和右选择，并进行了基本测试和性能测试。

--- src/main/java/com/moon/avltree/AVLTree.java
+++ src/main/java/com/moon/avltree/AVLTree.java
@@ -1,6 +1,6 @@
 package com.moon.avltree;
 
-import com.moon.map.Map;
+import java.util.ArrayList;
 
 /**
  * @author Chanmoey
@@ -14,7 +14,7 @@ public class AVLTree<K extends Comparable<K>, V> {
         public Node left;
         public Node right;
         public int height;
-        
+
 
         public Node(K key, V value) {
             this.key = key;
@@ -73,12 +73,79 @@ public class AVLTree<K extends Comparable<K>, V> {
         // 计算平衡因子
         int balanceFactor = getBalanceFactor(node);
 
-        // 不再平衡
-        if (Math.abs(balanceFactor) > 1) {
-            System.out.println("unbalance: " + balanceFactor);
+        // 平衡维护
+        // 左孩高于右孩 LL
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
+            // 右旋转
+            return rightRotate(node);
+        }
+
+        // 右孩高于左孩 RR
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+
+        // LR：新节点插入到不平衡阶段的左孩子的右孩子，导致了不平衡
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
 
         return node;
+    }
+
+    /**
+     *
+     * 对节点y进行向右旋转操作，返回旋转后的新的根节点x
+     *         y                                 x
+     *        / \                               / \
+     *       x   T4      向右旋转 (y)           z    y
+     *      / \        -------------->       / \   / \
+     *     z   T3                           T1 T2 T3  T4
+     *    / \
+     *   T1  T2
+     */
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node t3 = x.right;
+
+        // 右旋转
+        x.right = y;
+        y.left = t3;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
+    }
+
+    /**
+     * 对节点y进行向左旋转操作，返回旋转后的新的根节点x
+     *   y                                 x
+     *  / \                               / \
+     * T1  x      向左旋转 (y)            y    z
+     *    / \     -------------->      / \   / \
+     *   T2  z                        T1 T2 T3  T4
+     *      / \
+     *     T1 T2
+     */
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node t2 = x.left;
+
+        // 左选择
+        x.left = y;
+        y.right = t2;
+
+        // 更新height
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return x;
     }
 
     public boolean contains(K key) {
@@ -107,9 +174,11 @@ public class AVLTree<K extends Comparable<K>, V> {
     public void set(K key, V newValue) {
         Node node = getNode(root, key);
         if (node == null) {
-            throw new IllegalArgumentException(key + " doesn't exist");
+            add(key, newValue);
+        } else {
+            node.value = newValue;
         }
-        node.value = newValue;
+
     }
 
     public int getSize() {
@@ -118,6 +187,51 @@ public class AVLTree<K extends Comparable<K>, V> {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 是否是二分搜索树
+     *
+     * @return
+     */
+    public boolean isBST() {
+        ArrayList<K> keys = new ArrayList<>();
+        inOrder(root, keys);
+
+        for (int i = 1; i < keys.size(); i++) {
+            if (keys.get(i - 1).compareTo(keys.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void inOrder(Node node, ArrayList<K> keys) {
+
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
+    }
+
+    public boolean isBalance() {
+        return isBalance(root);
+    }
+
+    private boolean isBalance(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
+        }
+
+        return isBalance(node.left) && isBalance(node.right);
     }
 
     public V remove(K key) {

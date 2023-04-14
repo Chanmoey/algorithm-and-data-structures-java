44e23ec03d5592f00272adf65eb8bd59c791c8c3
Chanmoey <relijifa@gmail.com>
2023-04-14 12:33:53 +0800

    打印int的二进制形式。

--- src/main/java/com/moon/math/PrintBinary.java
+++ src/main/java/com/moon/math/PrintBinary.java
@@ -9,6 +9,7 @@ public class PrintBinary {
     private PrintBinary(){}
 
     public static void printIntBinary(int num) {
+        System.out.print(num + ": ");
         for (int i = 31; i >= 0; i--) {
             System.out.print((num >> i) & 1);
         }

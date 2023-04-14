49fa214b56865d23366d523ae30e1fab6b23d36f
Chanmoey <relijifa@gmail.com>
2023-04-14 11:15:18 +0800

    只用单链表实现LRU Cache。

---
+++ src/test/java/com/moon/cachereplacement/LRUJustLinkedListTest.java
@@ -0,0 +1,25 @@
+package com.moon.cachereplacement;
+
+import org.junit.Test;
+
+/**
+ * @author Chanmoey
+ * @date 2023年04月14日
+ */
+public class LRUJustLinkedListTest {
+
+    @Test
+    public void test() {
+        LRUJustLinkedList<Integer, Integer> lruCache = new LRUJustLinkedList<>(4);
+
+        lruCache.put(1, 1);
+        System.out.println(lruCache);
+        lruCache.put(2, 2);
+        System.out.println(lruCache.find(3));
+        lruCache.put(3, 3);
+        lruCache.put(4, 4);
+        System.out.println(lruCache);
+        lruCache.put(5, 5);
+        System.out.println(lruCache);
+    }
+}
